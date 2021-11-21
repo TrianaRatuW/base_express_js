ed6b23d487f7cd9c87843de0a8edee7523072ef8
Triana Ratu Wulandari <94730057+TrianaRatuW@users.noreply.github.com>
2021-11-22 00:47:23 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -15,9 +15,20 @@ bot.onText(/\/start/, (msg) => {
     bot.sendMessage(
         global_msg_id,
         `hello ${msg.chat.first_name}, welcome...\n
+        next >> /menu
+        `
+    );
+});
+
+bot.onText(/\/menu/, (msg) => {
+    global_msg_id = msg.chat.id;
+    bot.sendMessage(
+        global_msg_id,
+        `
         mau ngapain nih?
-        1. click /show_url
-        2. click /sensor`
+        1. /sensor
+        2. /show_url
+        `
     );
 });
 
